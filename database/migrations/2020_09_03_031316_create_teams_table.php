fa970cab1f1886bf08999da193ea3c2ce12bec94
Ariel <ariel@MacBook-Pro-de-Ariel.local>
2020-10-08 08:04:23 -0600

    wip

--- database/migrations/2020_09_03_031316_create_teams_table.php
+++ database/migrations/2020_09_03_031316_create_teams_table.php
@@ -15,28 +15,28 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->string('status', 10)->nullable();
+            $table->string('status')->nullable();
             $table->string('name');
             $table->string('slug');
-            $table->string('category', 50);
+            $table->string('category');
             $table->longText('description');
             $table->string('public');
             $table->integer('beneficiaries');
             $table->longText('impact');
-            $table->string('legal_representative', 50);
-            $table->string('tax_number', 50);
-            $table->string('office_address', 50);
+            $table->string('legal_representative');
+            $table->string('tax_number');
+            $table->string('office_address');
             $table->longText('use_of_funds');
-            $table->string('contact', 50);
-            $table->string('contact_phone', 50);
-            $table->string('contact_email', 64);
+            $table->string('contact');
+            $table->string('contact_phone');
+            $table->string('contact_email');
             $table->string('logo')->nullable();
             $table->string('banner')->nullable();
             $table->string('promotional_video')->nullable();
             $table->string('facebook_account')->nullable();
             $table->string('twitter_account')->nullable();
             $table->string('instagram_account')->nullable();
-            $table->string('theme', 30)->nullable()->default('columns');
+            $table->string('theme')->nullable()->default('columns');
             $table->string('bank');
             $table->string('account_number');
             $table->string('country');
