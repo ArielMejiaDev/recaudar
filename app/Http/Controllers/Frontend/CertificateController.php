4351ebca36445bc358b2d3d8998d66815727913f
Ariel <ariel@MacBook-Pro-de-Ariel.local>
2020-10-08 15:33:49 -0600

    wip

--- app/Http/Controllers/Frontend/CertificateController.php
+++ app/Http/Controllers/Frontend/CertificateController.php
@@ -7,6 +7,7 @@ use App\Models\Team;
 use App\Models\Transaction;
 use App\Services\DateToStringFormatter;
 use App\Services\SEO\TeamPageSeoService;
+use App\Services\SEO\TeamProfilePageSeoService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 
@@ -14,7 +15,7 @@ class CertificateController extends Controller
 {
     public function __invoke(Request $request, Team $team, Transaction $transaction)
     {
-        (new TeamPageSeoService)->execute();
+        (new TeamProfilePageSeoService)->execute($team);
         $certificateDate = (new DateToStringFormatter)->make($transaction->created_at);
         return view('certificate', compact('team', 'transaction', 'certificateDate'));
     }
